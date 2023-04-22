bb185570f4af50b592f3b222f3bee946572b02d3
Ariz Hernandez <arishassam@gmail.com>
2023-04-21 23:56:01 -0600

    Fix roles service

--- insert-values.sql
+++ insert-values.sql
@@ -26,5 +26,10 @@ VALUES ('INVITADO',
 
 INSERT INTO proyecto_ar.dbo.USUARIO (nombre, apellido, usuario, password, id_rol, fecha_crea, activo,
                                      usuario_mod, usuario_crea, fecha_mod, codigo)
-VALUES ( 'Ariz', 'Hernandez', 'ARZS', '123', 1, '2023-04-21 20:58:52.000', 1, '1', '1',
+VALUES ( 'Ariz', 'Hernandez', 'ARZS', '123', 1, '2023-04-21 20:58:52.000', 1, '', '',
+        '2023-04-21 20:59:01.000', '123');
+
+INSERT INTO proyecto_ar.dbo.USUARIO (nombre, apellido, usuario, password, id_rol, fecha_crea, activo,
+                                     usuario_mod, usuario_crea, fecha_mod, codigo)
+VALUES ( 'ar', 'test', 'TESTAR', '123', 1, '2023-04-21 20:58:52.000', 1, 'Ariz', 'Ariz',
         '2023-04-21 20:59:01.000', '123');
